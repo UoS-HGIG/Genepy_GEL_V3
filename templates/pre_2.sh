2bac53a90580aea67268da8263cd414e10c30ed1
parsboy1987 <165066453+parsboy1987@users.noreply.github.com>
2026-06-26 17:19:51 +0100

    Update pre_2.sh

--- templates/pre_2.sh
+++ templates/pre_2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 ###zcat f5.vcf.gz | awk 'BEGIN { OFS="\t" } !/^#/ { for (i = 10; i <= NF; i++) { $i = substr($i, 1, 3) } print }' > c6
 ###awk 'BEGIN { OFS="\t" } !/^#/ { for (i = 10; i <= NF; i++) { $i = substr($i, 1, 3) } print }' f5.vcf > c6
-> meta_CADDALL.txt
+####> meta_CADDALL.txt
 > meta_CADD15.txt
 > meta_CADD20.txt
-region=$1
-echo "$region"
-mkdir -p "metafiles15_${region}"
-mkdir -p "metafiles20_${region}"
-mkdir -p "metafilesALL_${region}"
-touch "metafilesALL_${region}/1.txt"
-touch "metafiles15_${region}/1.txt"
-touch "metafiles20_${region}/1.txt"
+####region=$1
+echo "${shard_num}_${subshard_num}"
+mkdir -p "metafiles15_${shard_num}_${subshard_num}"
+mkdir -p "metafiles20_${shard_num}_${subshard_num}"
+mkdir -p "metafilesALL_${shard_num}_${subshard_num}"
+touch "metafilesALL_${shard_num}_${subshard_num}/1.txt"
+touch "metafiles15_${shard_num}_${subshard_num}/1.txt"
+touch "metafiles20_${shard_num}_${subshard_num}/1.txt"
 
 
-cp header_meta meta_CADDALL.txt
+####cp header_meta meta_CADDALL.txt
 cp header_meta meta_CADD15.txt
 cp header_meta meta_CADD20.txt
 
@@ -43,38 +43,38 @@ if $aligned; then
 else
     echo "C* Files do not have the same number of lines."
 fi
-paste c1 c2 c3 c4 c5 c6 >> meta_CADDALL.txt
+####paste c1 c2 c3 c4 c5 c6 >> meta_CADDALL.txt
 paste c1 c2 c3 c4 c5a c6 >> meta_CADD15.txt
 paste c1 c2 c3 c4 c5b c6 >> meta_CADD20.txt
-mv c6 metafilesALL_${region}/.
-empty_count=$(awk -F'\t' 'NR > 1 && ($17 == "" || $17 == ".")  && $4 != "*"' meta_CADDALL.txt | wc -l)
+mv c6 metafiles15_${shard_num}_${subshard_num}/.
+empty_count=$(awk -F'\t' 'NR > 1 && ($17 == "" || $17 == ".")  && $4 != "*"' meta_CADD15.txt | wc -l)
 
 if [[ "$empty_count" -eq 0 ]]; then
-    echo "MetaCADD_ALL >>>> All rows have values in the SCORE1 (column 17)."
+    echo "MetaCADD_15 >>>> All rows have values in the SCORE1 (column 17)."
 else
-    echo "MetaCADD_ALL >>>> Found $empty_count rows with missing values in SCORE1 (column 17)."
+    echo "MetaCADD_15 >>>> Found $empty_count rows with missing values in SCORE1 (column 17)."
 fi
 ##CADD_all#
-while read gene;
-    do
-        
-        cp header_meta "metafilesALL_${region}/${gene}_CADDALL.meta"
-        grep -a -w "$gene" meta_CADDALL.txt >> "metafilesALL_${region}/${gene}_CADDALL.meta"
-        ###bgzip ${gene}_CADDALL.meta
-    done < gene.lst
-
+####while re#ad gene;
+####    do
+####        
+####        cp header_meta "metafilesALL_${region}/${gene}_CADDALL.meta"
+####        grep -a -w "$gene" meta_CADDALL.txt >> "metafilesALL_${region}/${gene}_CADDALL.meta"
+####        ###bgzip ${gene}_CADDALL.meta
+####    done < gene.lst
+####
 ##CADD_15
 while read gene;
     do
-        cp header_meta "metafiles15_${region}/${gene}_CADD15.meta"
-        grep -a -w "$gene" meta_CADD15.txt >> "metafiles15_${region}/${gene}_CADD15.meta"
+        cp header_meta "metafiles15_${shard_num}_${subshard_num}/${gene}_CADD15.meta"
+        grep -a -w "$gene" meta_CADD15.txt >> "metafiles15_${shard_num}_${subshard_num}/${gene}_CADD15.meta"
         ###bgzip ${gene}_CADD15.meta
     done < gene.lst
 
 ##CADD_20
 while read gene;
     do
-        cp header_meta "metafiles20_${region}/${gene}_CADD20.meta"
-        grep -a -w "$gene" meta_CADD20.txt >> "metafiles20_${region}/${gene}_CADD20.meta"
+        cp header_meta "metafiles20_${shard_num}_${subshard_num}/${gene}_CADD20.meta"
+        grep -a -w "$gene" meta_CADD20.txt >> "metafiles20_${shard_num}_${subshard_num}/${gene}_CADD20.meta"
         ###bgzip ${gene}_CADD20.meta
    done < gene.lst
